b9a2ac1f2e227744c61b42a9dbc90b0c03040063
ndmik <255708774+ndmik-dev@users.noreply.github.com>
2026-04-25 07:04:39 +0300

    add project skeleton

---
+++ src/main/java/io/ndmik/tsparser/TechstarsJobParserApplication.java
@@ -0,0 +1,13 @@
+package io.ndmik.tsparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechstarsJobParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechstarsJobParserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/ndmik/tsparser/TechstarsJobParserApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ndmik.tsparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechstarsJobParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
